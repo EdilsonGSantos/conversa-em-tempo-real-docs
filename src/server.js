a8674d95eb12e3bd04eb9e2dffae9c657c8dbd07
Edilson Gomes <edilson.gs1602@gmail.com>
2024-04-19 16:20:35 -0300

    socketio configuration

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import url from "url";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const port = process.env.port || 3000;
@@ -10,4 +12,12 @@ const publicDirectory = path.join(currentPath, "../..", "public");
 
 app.use(express.static(publicDirectory));
 
-app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+const serverHttp = http.createServer(app);
+
+serverHttp.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+
+const io = new Server(serverHttp);
+
+io.on("connection", () => {
+  console.log("Um cliente se conectou!");
+});
